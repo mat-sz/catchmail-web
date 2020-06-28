b9f9dc662a1ae8e45324448b8cf01290c59dd959
Mat Sz <contact@matsz.dev>
2020-06-28 16:32:51 +0200

    added message body preview

--- src/components/EmailListItem.tsx
+++ src/components/EmailListItem.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import TimeAgo from 'react-timeago';
+import { sanitize } from 'react-letter';
 import clsx from 'clsx';
 
 import { selectEmailAction } from '../actions/emails';
@@ -11,10 +12,28 @@ export interface EmailListItemProps {
   email: EmailModel;
 }
 
+function truncate(str: string, length: number, useWordBoundary = true) {
+  if (str.length <= length) {
+    return str;
+  }
+
+  const subString = str.substr(0, length - 1);
+  return useWordBoundary
+    ? subString.substr(0, subString.lastIndexOf(' '))
+    : subString;
+}
+
 const EmailListItem: React.FC<EmailListItemProps> = ({ email }) => {
   const selectedEmail = useSelector((state: StateType) => state.selectedEmail);
   const dispatch = useDispatch();
   const selectEmail = () => dispatch(selectEmailAction(email.id));
+  const text = useMemo(() => {
+    const sanitized = sanitize(email?.html || '', undefined, {
+      dropAllHtmlTags: true,
+    });
+    const doc = new DOMParser().parseFromString(sanitized, 'text/html');
+    return truncate(doc.body.innerText, 200);
+  }, [email]);
 
   return (
     <li
@@ -29,6 +48,7 @@ const EmailListItem: React.FC<EmailListItemProps> = ({ email }) => {
         <TimeAgo date={email.date} />
       </span>
       <span className="subject">{email.subject}</span>
+      <div className="body">{text}&hellip;</div>
     </li>
   );
 };
